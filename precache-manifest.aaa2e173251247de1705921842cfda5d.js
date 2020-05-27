d8d1b40ddda7da27c47ddc367c29e527bf0f28ab
Alex Hughes <alexhughes@AlexMBPro.local>
2020-05-27 12:21:25 -0700

    Updates

---
+++ precache-manifest.aaa2e173251247de1705921842cfda5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a786ca8e02075c8df60b1578c6c4f874",
+    "url": "/Alex-Hughes-fe-test-css/index.html"
+  },
+  {
+    "revision": "93d5a75a7e39cc94bebe",
+    "url": "/Alex-Hughes-fe-test-css/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0b400f37da72e771833",
+    "url": "/Alex-Hughes-fe-test-css/static/js/2.937b9098.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Alex-Hughes-fe-test-css/static/js/2.937b9098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d5a75a7e39cc94bebe",
+    "url": "/Alex-Hughes-fe-test-css/static/js/main.8d28a314.chunk.js"
+  },
+  {
+    "revision": "fe5f47d2afa54e8eea9c",
+    "url": "/Alex-Hughes-fe-test-css/static/js/runtime-main.3b715c10.js"
+  }
+]);
